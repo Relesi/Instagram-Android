a39ce5293501a31e27b9e31d2b060fc540dc6438
Renato Lessa <renatolessa.2011@hotmail.com>
2017-06-14 08:36:24 -0300

    Inserindo código de teste de vvalidação de dados

--- ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -37,18 +37,7 @@ public class StarterApplication extends Application {
     );
 
 
-      // Teste de configuração do App
-      ParseObject pontuacao = new ParseObject("Pontuacao");
-      pontuacao.put("pontos", 100);
-      pontuacao.saveInBackground(new SaveCallback() {
-          public void done(ParseException e) {
-              if (e == null) {
-                  Log.i("TesteExecucao", "Salvo com sucesso!!!");
-              } else {
-                  Log.i("TesteExecucao", "Falha ao salvar os dados!!!");
-              }
-          }
-      });
+    
 
 
       ParseUser.enableAutomaticUser();
---
+++ testeValidacaoDados.java
@@ -0,0 +1,12 @@
+  // Teste de configuração do App
+      ParseObject pontuacao = new ParseObject("Pontuacao");
+      pontuacao.put("pontos", 100);
+      pontuacao.saveInBackground(new SaveCallback() {
+          public void done(ParseException e) {
+              if (e == null) {
+                  Log.i("TesteExecucao", "Salvo com sucesso!!!");
+              } else {
+                  Log.i("TesteExecucao", "Falha ao salvar os dados!!!");
+              }
+          }
+      });
