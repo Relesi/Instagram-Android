199605a9a5086ce354a97e5cd191cc72ae651295
Renato Lessa <renatolessa.2011@hotmail.com>
2017-06-16 16:05:47 -0300

    Cadastro login e logout

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -17,11 +17,14 @@ import android.view.MenuItem;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
+import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 import java.util.List;
 
@@ -33,66 +36,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-   /* ParseObject pontuacao = new ParseObject("Pontuacao");
-    pontuacao.put("nome","Javaneiro");
-    pontuacao.put("pontos" , 150);
-    pontuacao.saveInBackground(new SaveCallback() {
-      @Override
-      public void done(ParseException e) {
-        if(e == null ){//Não temos erro
-          Log.i("salvarPontos", "Dados salvos com sucesso");
-        }else{
-          Log.i("salvarPontos", "Dados salvos com sucesso");
 
-        }
-      }
-    });*/
+        /**
+         * Cadastro de Usuários
+        **/
+       /* ParseUser usuario = new ParseUser();
+        usuario.setUsername("renatolessa");
+        usuario.setPassword("123456");
+        usuario.setEmail("renatolessa.2011@hotmail.com");
 
-    /*ParseQuery<ParseObject> consulta = ParseQuery.getQuery("Pontuacao");
-    consulta.getInBackground("GSGDnl1KBG", new GetCallback<ParseObject>() {
-      @Override
-      public void done(ParseObject object, ParseException e) {
+        //Cadastrar
 
-        if(e == null ){//Não temos erro
-          object.put("pontos", 1200);
-          object.saveInBackground();
-        }else{
-          Log.i("salvarPontos", "Dados salvos com sucesso");
-
-        }
-      }
-    });*/
-        ParseQuery<ParseObject> filtro = ParseQuery.getQuery("Pontuacao");
-
-        //Aplicando filtors na listagem de objetos
-
-        //filtro.whereGreaterThan("pontos", 800);
-         filtro.whereGreaterThanOrEqualTo("pontos", 800);
-        //filtro.whereLessThan("pontos", 500);
-        //filtro.whereStartsWith("nome", "Ja");
-        //filtro.addAscendingOrder("pontos");
-          filtro.addDescendingOrder("pontos");
-          filtro.setLimit(1);
-
-        //Listar os dados
-        filtro.findInBackground(new FindCallback<ParseObject>() {
+        usuario.signUpInBackground(new SignUpCallback() {
             @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-
-                if (e == null) {//Efetuar Listagem sem erros
-
-                   for(ParseObject object: objects){
-
-                       Log.i("Listar dados", " objetos - Nome: " + object.get("nome") + " pontos: " + object.get("pontos") );
-                   }
-
-                } else {//Erro ao listar
-
-                    Log.i("Listar dados", " erro ao Listar os objetos -" + e.getMessage());
+            public void done(ParseException e) {
+                if(e == null){//Deu certo
+                    Log.i("cadastrousuario", "Sucesso ao cadastrar usuáio ");
+                }else {//Erro
+                    Log.i("cadastrousuario", "Erro ao cadastrar usuáio -  " + e.getMessage());
                 }
             }
+        });*/
 
+        /***
+         * Verificar usuário logado
+         */
+
+        //Deslogado
+
+        /*ParseUser.logOut();
+
+        //Verificar Logado
+
+        if(ParseUser.getCurrentUser() != null){//Logado
+            Log.i("LoginUsuario", "Usuário está logado");
+        }else{//Não logado
+            Log.i("LoginUsuario", "Usuário não está logado");
+        }*/
+
+        /**
+         * Fazer login de usuário
+         */
+        ParseUser.logInInBackground("renatolessa", "123456", new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+
+                if(e == null){
+
+                    Log.i("VerificaLoginUsuario", "Login realizado com sucesso");
+                }else{
+
+                    Log.i("VerificaLoginUsuario", "Erro ao fazer login do usuário. " + e.getMessage());
+                }
+            }
         });
+
     }
 
 }
