8fc9e76d89bae6ef804e1d8412d72e47cadd7dba
Renato Lessa <renatolessa.2011@hotmail.com>
2017-06-16 00:06:04 -0300

    Filtrando dados

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
@@ -22,15 +23,17 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    ParseObject pontuacao = new ParseObject("Pontuacao");
+   /* ParseObject pontuacao = new ParseObject("Pontuacao");
     pontuacao.put("nome","Javaneiro");
     pontuacao.put("pontos" , 150);
     pontuacao.saveInBackground(new SaveCallback() {
@@ -43,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
       }
-    });
+    });*/
 
     /*ParseQuery<ParseObject> consulta = ParseQuery.getQuery("Pontuacao");
     consulta.getInBackground("GSGDnl1KBG", new GetCallback<ParseObject>() {
@@ -59,6 +62,37 @@ public class MainActivity extends AppCompatActivity {
         }
       }
     });*/
-  }
+        ParseQuery<ParseObject> filtro = ParseQuery.getQuery("Pontuacao");
+
+        //Aplicando filtors na listagem de objetos
+
+        //filtro.whereGreaterThan("pontos", 800);
+         filtro.whereGreaterThanOrEqualTo("pontos", 800);
+        //filtro.whereLessThan("pontos", 500);
+        //filtro.whereStartsWith("nome", "Ja");
+        //filtro.addAscendingOrder("pontos");
+          filtro.addDescendingOrder("pontos");
+          filtro.setLimit(1);
+
+        //Listar os dados
+        filtro.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+
+                if (e == null) {//Efetuar Listagem sem erros
+
+                   for(ParseObject object: objects){
+
+                       Log.i("Listar dados", " objetos - Nome: " + object.get("nome") + " pontos: " + object.get("pontos") );
+                   }
+
+                } else {//Erro ao listar
+
+                    Log.i("Listar dados", " erro ao Listar os objetos -" + e.getMessage());
+                }
+            }
+
+        });
+    }
 
 }
