3537b24cddc954178c36d7bb607b32a45d0fd0f3
Renato Lessa <renatolessa.2011@hotmail.com>
2017-07-27 00:12:18 -0300

    Criando compartilhamento de fotos

--- ParseStarterProject/src/main/java/com/parse/starter/activity/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/MainActivity.java
@@ -9,7 +9,10 @@
 package com.parse.starter.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
@@ -19,12 +22,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -34,8 +39,11 @@ import com.parse.starter.R;
 import com.parse.starter.adapter.TabsAdapter;
 import com.parse.starter.util.SlidingTabLayout;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -49,22 +57,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Configura Toolbar
+        //Configura toolbar
         toolbarPrincipal = (Toolbar) findViewById(R.id.toolbar_principal);
-        toolbarPrincipal.setLogo(R.drawable.instagramlogo);
-        setSupportActionBar(toolbarPrincipal);
+        toolbarPrincipal.setLogo( R.drawable.instagramlogo );
+        setSupportActionBar( toolbarPrincipal );
 
-        //Configura Abas
+        //Configura abas
         slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tab_main);
         viewPager = (ViewPager) findViewById(R.id.view_pager_main);
 
-        //configurar Adapter
-        TabsAdapter tabsAdapter = new TabsAdapter(getSupportFragmentManager(), this);
+        //configurar adapter
+        TabsAdapter tabsAdapter = new TabsAdapter( getSupportFragmentManager(), this );
+        viewPager.setAdapter( tabsAdapter );
         slidingTabLayout.setCustomTabView(R.layout.tab_view, R.id.text_item_tab);
-        viewPager.setAdapter(tabsAdapter);
         slidingTabLayout.setDistributeEvenly(true);
-        slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.cinzaEscuro));
-        slidingTabLayout.setViewPager(viewPager);
+        slidingTabLayout.setSelectedIndicatorColors( ContextCompat.getColor(this, R.color.cinzaEscuro) );
+        slidingTabLayout.setViewPager( viewPager );
+
     }
 
     @Override
@@ -73,27 +82,91 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return true;
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()){
+        switch ( item.getItemId() ){
             case R.id.action_sair:
-
+                //fazer algo
                 deslogarUsuario();
-
                 return true;
             case R.id.action_configuracoes:
                 return true;
             case R.id.action_compartilhar:
+                compartilharFoto();
                 return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
+
+    private void compartilharFoto(){
+
+        Intent intent = new Intent( Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Testar processo de retorno dos dados
+        if( requestCode==1 && resultCode == RESULT_OK && data != null ){
+
+            //recuperar local do recurso
+            Uri localImagemSelecionada = data.getData();
+
+            //recupera a imagem do local que foi selecionada
+            try {
+                Bitmap imagem = MediaStore.Images.Media.getBitmap( getContentResolver(), localImagemSelecionada );
+
+                //comprimir no formato PNG
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                imagem.compress(Bitmap.CompressFormat.PNG, 75, stream);
+                //Cria um array de bytes da imagem
+                byte[] byteArray = stream.toByteArray();
+
+                //Criar um arquivo com formato próprio do parse
+                SimpleDateFormat dateFormat = new SimpleDateFormat("ddmmaaaahhmmss");
+                String nomeImagem = dateFormat.format( new Date() );
+                ParseFile arquivoParse = new ParseFile(nomeImagem +"imagem.png", byteArray);
+
+                //Monta objeto para salvar no parse
+                ParseObject parseObject = new ParseObject("Imagem");
+                //parseObject.put("username", ParseUser.getCurrentUser().getUsername() );
+                parseObject.put("imagem", arquivoParse );
+
+                //salvar os dados
+                parseObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if( e==null ){//sucesso
+                            Toast.makeText(getApplicationContext(), "Sua imagem foi postada!!!", Toast.LENGTH_LONG ).show();
+                        }else{//erro
+                            Toast.makeText(getApplicationContext(), "Erro ao postar sua imagem - Tente novamente!",
+                                    Toast.LENGTH_LONG ).show();
+                        }
+
+                    }
+                });
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
     private void deslogarUsuario(){
         ParseUser.logOut();
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
 }
